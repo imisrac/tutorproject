f2f5445be2b8b1985692a368232dbeac2dc1e0e6
Németh Imre <imisrac@gmail.com>
2012-04-03 14:41:44 +0200

    Add a title helper.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+  end
+
+  def help
+    @title = "Help"
+  end
+
+  def about
+    @title = "About Us"
   end
 
   def contact
+    @title = "Contacts"
   end
 end
