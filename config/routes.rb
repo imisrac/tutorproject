0445cfb34e1d3df61232ec3677e671ddf59ca90e
Németh Imre <imisrac@gmail.com>
2012-04-28 18:57:06 +0200

    Finish sign in. Cucumber leaved out.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Tutorproject::Application.routes.draw do
   resources :users
-
-  root to: 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  root to: 'static_pages#home'
 
   match '/help', to: 'static_pages#help'
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,9 +23,10 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
-  it {should be_valid}
+  it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -106,5 +107,10 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
